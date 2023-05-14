1a267413213ad4ee3254d29015e3dba01ee89265
wasim altayyan <wasimtayyan@gmail.com>
2023-05-14 17:25:19 +0300

    assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+class CAt {
+    constructor(name, sex){
+        this.name = name,
+        this.sex = sex
+    }
+    get speak (){
+        return `${this.name} says meow!`
+    }
+}
+class Dog {
+    constructor(name,sex){
+        this.name = name,
+        this.sex = sex
+    }
+    get speak(){
+        return  `${this.name} says woof!`
+    }
+}
+class Bird {
+    constructor(name,sex){
+        this.name = name,
+        this.sex = sex
+    }
+    get speak(){
+        if(this.sex === 'male'){
+            return `it's me! ${this.name}, the parrot`
+        }else{
+            return  `${this.name} says squawk!`
+        }
+    }
+}
+const rose = new CAt('rose','female')
+const rory = new Bird('rory', 'male')
+const flappy = new Bird('flappy', 'female')
+console.log(rose.speak)
+console.log(rory.speak)
+console.log(flappy.speak)
